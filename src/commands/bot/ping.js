c3b4b538ffffedabd297805b912009aa7e6b9da5
uwuv3 <a1bilg1sayar@outlook.com>
2022-08-13 18:35:01 +0300

    Update v5.01

---
+++ src/commands/bot/ping.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "boost-log",
+    aliases: [""],
+    permission: ["SEND_MESSAGES"],
+    cooldown: 5000,
+    adminOnly: false,
+    /**
+     *
+     * @param {Message} message
+     * @param {Client} client
+     * @param {String[]} args
+     */
+    run: async (message, client, args) => {
+        message.reply({content:`${client.ws.ping}ms`})
+    }}

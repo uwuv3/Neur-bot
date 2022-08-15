ab9d823dd57a0dd4e0791c4117b7f822934dbc98
uwuv3 <a1bilg1sayar@outlook.com>
2022-08-15 13:48:08 +0300

    Kanal seçme özelliği geldi

--- src/commands/mod/boost-log.js
+++ src/commands/mod/boost-log.js
@@ -15,22 +15,60 @@ module.exports = {
    * @param {String[]} args
    */
   run: async (message, client, args) => {
-    if (args == "aç") {
-      new db({
+    if (args[0] == "aç") {
+      const { channelID } = (await db.findOne({
         guildID: message.guild.id,
-        channelID: message.channel.id,
-      }).save();
-      message.reply({
-        embeds: [
-          new MessageEmbed()
-            .setColor("GREEN")
-            .setDescription(
-              emotes.tik +
-                `Boost log kanalı başarıyla ${message.channel} ayarlandı!`
-            ),
-        ],
-      });
-    } else if (args == "kapat") {
+      })) || {
+        channelID: null,
+      };
+      let id;
+      if (channelID) id = channelID;
+      let name;
+      if (id) name = await client.channels.cache.get(id).name;
+      if (!name) name = "null";
+      const button = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId(message.author.id)
+          .setDisabled(true)
+          .setStyle("SECONDARY")
+          .setLabel(`Şu anki kanal: ${name}`)
+      );
+      if (channelID) {
+        message.reply({
+          content:
+            emotes.carpi +
+            "Databaseye göre bu sunucya ait bir boostlog kanalı var",
+          components: [button],
+          allowedMentions: { repiledUser: false },
+        });
+      } else {
+        let kanal = message.mentions.channels.first();
+        if (!kanal)
+          return message.reply({
+            content: emotes.carpi + "Bir kanal seç!",
+            allowedMentions: { repiledUser: false },
+          });
+        if (!kanal.type === "GUILD_TEXT")
+          return message.reply({
+            content: emotes.carpi + "Seçtiğin kanal bir yazı kanalı olmalı",
+            allowedMentions: { repiledUser: false },
+          });
+
+        new db({
+          guildID: message.guild.id,
+          channelID: kanal.id,
+        }).save();
+        message.reply({
+          embeds: [
+            new MessageEmbed()
+              .setColor("GREEN")
+              .setDescription(
+                emotes.tik + `Boost log kanalı başarıyla ${kanal} ayarlandı!`
+              ),
+          ],
+        });
+      }
+    } else if (args[0] == "kapat") {
       let deneme = await db.findOne({ guildID: message.guild.id });
       if (!deneme)
         return message.reply({
@@ -69,7 +107,7 @@ module.exports = {
           .setLabel(`Şu anki kanal: ${name}`)
       );
       message.reply({
-        content: "Bir değer seç `aç`**/**`kapat`",
+        content: emotes.carpi + "Bir değer seç `aç`**/**`kapat`",
         components: [button],
         allowedMentions: { repiledUser: false },
       });

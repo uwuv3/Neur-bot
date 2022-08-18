688f2aa5b1d7ddfda5820592d6a78303e60cebc9
uwuv3 <a1bilg1sayar@outlook.com>
2022-08-18 21:25:32 +0300

    Dm mesaj

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const config = {
   mongodburl:
     "mongodb+srv://uwuv3:rkzgkHg8FMcL2mal@cluster0.hktzrvl.mongodb.net/?retryWrites=true&w=majority",
-  intents: 131071, //https://ziad87.net/intents/
+  intents:  131071, //https://ziad87.net/intents/
   prefix: "?",
+  dmmsgchnl: "977846930975633438",
   color: "DARK_RED",
   admins: [
     "984439714851479593", //N๏ ภค๓є#8841
---
+++ src/events/Client/dmmmsg.js
@@ -0,0 +1,18 @@
+const client = require("../../index");
+const { MessageEmbed } = require("discord.js");
+const { config } = require("../../../config");
+client.on("messageCreate", async (message) => {
+  if (message.channel.type === "DM") {
+    if(message.author.bot) return
+    const dmsg = new MessageEmbed()
+      .setTitle(`${client.user.username} - DM mesaj`)
+      .setColor(config.color)
+      .addFields([
+        { name: "Mesajı gönderen :", value: message.author.tag },
+        { name: "Gönderilen mesaj:", value: message.content },
+      ])
+      .setThumbnail(message.author.avatarURL());
+
+    client.channels.cache.get(config.dmmsgchnl).send({ embeds: [dmsg] });
+  }
+});
